6aa2470dea3b1630ee7bba322184c8f3fbbf2399
Ronaldo Guastalli <guastalli31@gmail.com>
2019-08-31 10:41:50 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/br/com/studies/cursomc/domanin/Categoria.java
+++ src/main/java/br/com/studies/cursomc/domanin/Categoria.java
@@ -5,6 +5,10 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 
@@ -12,8 +16,11 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = {"id"})
+@Entity
 public class Categoria implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 }
